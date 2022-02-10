d9943f57206f72384aee7b194c16f01e417426fd
iagosayar <iagocelestino@gmail.com>
2022-02-10 11:17:07 +0100

    Home changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,7 @@ export const environment = {
   production: true,
   movieApiUrl: 'https://api.themoviedb.org/3',
   movieApiKey: '5828e432298a837499ec6162b945c4db',
+  unsplashApiKey:'dZFmEsxb3hYVggj7WyRmXEyaI48thcM-eqyoLdJ3zks',
+  unsplashUrl:'https://api.unsplash.com',
   backendApiUrl: 'http://localhost:3977/api'
 };
